3077ecdb915dffbcc6dd371672ae1bdc384af1bf
juliandillmann <juliandillmann@air-von-julian>
2022-06-23 16:40:28 +0200

    JD-load from csv

--- ptp-lernkarten/src/model/DeckManager.java
+++ ptp-lernkarten/src/model/DeckManager.java
@@ -11,53 +11,78 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 
 public class DeckManager {
-private static HashMap<Integer, Deck> decks = new HashMap<Integer, Deck>();
 
-public DeckManager() {
-	
-}
-public static void addDeck(int index, Deck d) {
-	decks.put(index, d);
-}
+	private static HashMap<Integer, Deck> decks = new HashMap<Integer, Deck>();
+	private static Path pathDirectory;
 
-public static HashMap<Integer, Deck> getDecks() {
-	return decks;
-}
-public static void setDecks(HashMap<Integer, Deck> decks) {
-	DeckManager.decks = decks;
-}
+	public DeckManager() {
 
-public void getData(String path) {
-	String line = "";
-	HashMap<Integer, Deck> newdecks = new HashMap<Integer, Deck>();
-	Deck newdeck = new Deck("path");
-	try {
-		BufferedReader br = new BufferedReader(new FileReader(path));
-			while((line = br.readLine())!= null) {
-				String[] values = line.split(",");
-				newdeck.addFlashcard(new Flashcard(Integer.parseInt(values[0]),values[1], values[2]));
+	}
+
+	public static void addDeck(int index, Deck d) {
+		decks.put(index, d);
+	}
+
+	public static HashMap<Integer, Deck> getDecks() {
+		return decks;
+	}
+
+	public static void setDecks(HashMap<Integer, Deck> decks) {
+		DeckManager.decks = decks;
+	}
+
+	public static String[] findAllFilesInFolder(File folder) {
+		String[] paths = new String[folder.listFiles().length];
+		for (int i = 0; i < folder.listFiles().length; i++) {
+			paths[i] = folder.listFiles()[i].toString();
+		}
+		for (String s : paths) {
+			System.out.println(s);
+		}
+		System.out.println("--------------------------------------------------");
+		return paths;
+	}
+
+	public static void getData(File folder) {
+		String[] paths = findAllFilesInFolder(folder);
+		String line = "";
+//		HashMap<Integer, Deck> newdecks = new HashMap<Integer, Deck>();
+		for (int i = 0; i < paths.length; i++) {
+			Deck newdeck = new Deck(paths[i]);
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(paths[i]));
+				while ((line = br.readLine()) != null) {
+					String[] values = line.split(";");
+					newdeck.addFlashcard(new Flashcard(Integer.parseInt(values[0]), values[1], values[2]));
+				}
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
+			addDeck(i, newdeck);
+		}
+
 	}
-}
-public static String createDirectories() {
 
-	String path = System.getProperty("user.home");
-	Path pathDirectory = Paths.get(path, "decks");
-	try {
-		Files.createDirectories(pathDirectory);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public static void createDirectories() {
+
+		String path = System.getProperty("user.home");
+		pathDirectory = Paths.get(path, "decks");
+		try {
+			Files.createDirectories(pathDirectory);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-System.out.println(pathDirectory);	
 
-return pathDirectory.toString();	
-}
- 
-}
+	public static String getPathtoString() {
+		String path = System.getProperty("user.home");
+		pathDirectory = Paths.get(path, "decks");
+		return pathDirectory.toString();
+	}
 
+}
