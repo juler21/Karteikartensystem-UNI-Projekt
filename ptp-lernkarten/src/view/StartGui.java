ce75f9f4d98c2a5824fb6afef55dc3981c10f3c3
mark <mark@dhcp-172-21-66-49.wlan.uni-hamburg.de.local>
2022-06-23 14:11:14 +0200

    MS-Gui updates

--- ptp-lernkarten/src/view/StartGui.java
+++ ptp-lernkarten/src/view/StartGui.java
@@ -14,6 +14,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.CardLayout;
+import java.awt.GridLayout;
 
 public class StartGui extends JFrame {
 	
@@ -59,11 +60,23 @@ public class StartGui extends JFrame {
 		
 		JPanel decksCard = new JPanel();
 		startContentPane.add(decksCard, "name_2859201282041");
+		decksCard.setLayout(new GridLayout(0, 2, 0, 0));
+		JButton deckErstellenButton = new JButton("Deck Erstellen");
+		deckErstellenButton.setBounds(146, 93, 141, 29);
+		decksCard.add(deckErstellenButton);
+		deckErstellenButton.setFont(new Font("Ubuntu", Font.PLAIN, 13));
+		deckErstellenButton.setBackground(Color.GREEN);
+		deckErstellenButton.setForeground(new Color (2, 48, 89));
+		
+		JButton deckBearbeitenButton = new JButton("Deck Bearbeiten");
+		decksCard.add(deckBearbeitenButton);
+		deckBearbeitenButton.setFont(new Font("Ubuntu", Font.PLAIN, 13));
+		deckBearbeitenButton.setForeground(new Color (2, 48, 89));
 		
 		JPanel statistikCard = new JPanel();
 		startContentPane.add(statistikCard, "name_2893444114291");
 		
-		((CardLayout) startContentPane.getLayout()).show(startContentPane,"name_2859201282041");
+		((CardLayout) startContentPane.getLayout()).show(startContentPane,"name_2836609601500");
 		mainFrame.setVisible(true);
 		
 	}
