dbab61a157e346772d6226b2dce0bbd12ba02311
juliandillmann <juliandillmann@air-von-julian>
2022-06-23 16:40:51 +0200

    Merge branch 'main' of https://git.informatik.uni-hamburg.de/0sterkel/ptp22-do08-lernkarten.git into main

--- ptp-lernkarten/src/util/Main.java
+++ ptp-lernkarten/src/util/Main.java
@@ -1,10 +1,21 @@
 package util;
 
+import java.io.File;
+
+import model.DeckManager;
 import view.Gui;
 
 public class Main {
-	 public static void main (String[] args) {
-		 
-		 new Gui();
-	 }
+	public static void main(String[] args) {
+
+		new Gui();
+		File folder = new File(DeckManager.getPathtoString());
+		DeckManager.getData(folder);
+		System.out.println(DeckManager.getDecks().get(0).getDeckname());
+		System.out.println(DeckManager.getDecks().get(1).getDeckname());
+		System.out.println(DeckManager.getDecks().get(2).getDeckname());
+		System.out.println(DeckManager.getDecks().get(2).getDeckname());
+
+	}
+
 }
