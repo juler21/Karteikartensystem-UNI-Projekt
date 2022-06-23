41b515a284173fa605e0b3110aeaca0d5914be86
juliandillmann <juliandillmann@air-von-julian>
2022-06-23 12:08:37 +0200

    JD-Persistenz

--- ptp-lernkarten/src/controller/ButtonListener.java
+++ ptp-lernkarten/src/controller/ButtonListener.java
@@ -12,16 +12,16 @@ import view.Gui;
 import view.QAGUI;
 
 public class ButtonListener implements ActionListener {
-	
+
 	private JFrame gui;
 	private String cmd;
 	private int index;
 	private static Deck deck;
-	
+
 	public ButtonListener(JFrame gui, String cmd) {
 		this.gui = gui;
 		this.cmd = cmd;
-		
+
 		index = 1;
 	}
 
@@ -29,32 +29,27 @@ public class ButtonListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		doCommand(cmd);
 	}
+
 	private void doCommand(String cmd) {
-		if(cmd.equals("ok"))
-		{
+		if (cmd.equals("ok")) {
 			String question = ((QAGUI) gui).getQuestion().getText();
 			String answer = ((QAGUI) gui).getAnswer().getText();
-			
-			
+
 			deck.addFlashcard(new Flashcard(index, question, answer));
 			index++;
 			((QAGUI) gui).getQuestion().setText("");
 			((QAGUI) gui).getAnswer().setText("");
-		}
-		else if(cmd.equals("confirm")) {
+		} else if (cmd.equals("confirm")) {
 			System.out.println(DeckManager.getDecks().get(1).getDeckname());
-			for(Flashcard f: deck.getDeck()) {
+			for (Flashcard f : deck.getDeck()) {
 				System.out.println(f.getIndex());
 				System.out.println(f.getQuestion());
 				System.out.println(f.getAnswer());
 			}
-			DeckManager.createDirectories();
-			System.out.println(DeckManager.createDirectories().getAbsolutePath());
-			
-//			deck.loadInCSV();
-			
-		}
-		else if(cmd.equals("confirmDeck")) {
+
+			deck.loadInCSV();
+
+		} else if (cmd.equals("confirmDeck")) {
 			Deck newDeck = new Deck(((Gui) (gui)).getDeckName().getText());
 			deck = newDeck;
 			DeckManager.addDeck(index, newDeck);
--- ptp-lernkarten/src/util/Main.java
+++ ptp-lernkarten/src/util/Main.java
@@ -2,7 +2,7 @@ package util;
 
 import view.Gui;
 
-public class DemoDatabase {
+public class Main {
 	 public static void main (String[] args) {
 		 
 		 new Gui();
