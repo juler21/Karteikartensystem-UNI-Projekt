177a176d54a191aeb76cf10b167442f31a430341
juliandillmann <juliandillmann@localhost>
2022-08-10 11:06:40 +0200

    JD-Methodennamen geändert

--- ptp-lernkarten/src/util/Main.java
+++ ptp-lernkarten/src/util/Main.java
@@ -94,7 +94,7 @@ public class Main {
 	/*
 	 * 
 	 */
-	public void changeDirectory(String foldername) {
+	public void changeFoldername(String foldername) {
 		deckmanager.changeDirectory(foldername);
 	}
 
