8f4cd6e4d4c6bfa05cae46d02060405fc8f25332
juliandillmann <juliandillmann@localhost>
2022-08-12 11:24:48 +0200

    JD- Im model alles kommentiert

--- ptp-lernkarten/src/model/Deck.java
+++ ptp-lernkarten/src/model/Deck.java
@@ -11,25 +11,34 @@ import java.util.List;
 
 import util.UnvalidQAException;
 
+/**
+ * * PTP 22- Karteikartenlernprogramm Das Deck ist für die Verwaltung und
+ * Speicherung von Karteikarten zuständig.
+ * 
+ * @author J.Dillmann, M. Sterkel
+ */
 public class Deck {
 
-	private List<Flashcard> flashcardList = new ArrayList<Flashcard>();
+	private List<Flashcard> flashcardList = new ArrayList<Flashcard>(); // Liste von Karteikarten
+	private String deckname; // Deckname
 
-	private String deckname;
-
-	public Deck(String deck) {
-		deckname = deck;
+	/**
+	 * Im Konstruktor wird dem Deck der Deckname zugeordnet
+	 */
+	public Deck(String deckname) {
+		this.deckname = deckname;
 	}
 
-	private boolean checkQAText(String text) throws UnvalidQAException {
-
-		if (!text.isBlank() && text != null) {
-			return true;
-		} else {
-			throw new UnvalidQAException();
-		}
-	}
-
+	/**
+	 * Fügt dem Deck eine Karteikarte mit Frage und Antwort hinzu. Wirft eine
+	 * UnvalidQAException. Löscht die CSV Datei und lädt sie neu.
+	 * 
+	 * @param question Frage, welche hinzugefügt wird
+	 * @param answer   Antwort die der Karteikarte hinzugefügt wird
+	 * 
+	 * @throws UnvalidQAException wirft eine Exception, wenn mindestens ein Feld
+	 *                            null oder leer ist.
+	 */
 	void addFlashcard(String question, String answer) throws UnvalidQAException {
 		if (checkQAText(question) && checkQAText(answer)) {
 			Flashcard flash = new Flashcard(question, answer);
@@ -39,24 +48,50 @@ public class Deck {
 		}
 	}
 
+	/**
+	 * Löscht alle Karteikarten aus dem Deck.
+	 */
 	void deleteAllFlashcards() {
 		flashcardList.removeAll(flashcardList);
 	}
 
-	void loadFlashcard(Flashcard flash) {
-		flashcardList.add(flash);
+	/**
+	 * Fügt eine Karteikarte der Liste.
+	 * 
+	 * @param flashcard Karteikarte, welche hinzugefügt wird
+	 */
+	void loadFlashcard(Flashcard flashcard) {
+		flashcardList.add(flashcard);
 	}
 
+	/**
+	 * Gibt die Karteikarte aus dem Deck wieder.
+	 * 
+	 * @param index Position der Karteikarte in der Liste
+	 * @return Flashcard die entsprechende Karteikarte
+	 */
 	Flashcard getFlashcard(int index) {
 		return flashcardList.get(index);
 	}
 
+	/**
+	 * Löscht eine Karteikarte aus dem Deck und löscht die CSV Datei und lädt sie
+	 * neu
+	 * 
+	 * @param index Position der Karteikarte in der Liste
+	 */
 	void deleteFlashcard(int index) {
 		flashcardList.remove(index);
 		deleteDeckCSV();
 		saveDeckCSV();
 	}
 
+	/**
+	 * Sichert das Deck als CSV Datei in dem gewählten Ordner. der Deckname ist der
+	 * Dateiname von Der CSV Datei. Die einzelnen Karteikarten werden als Zeilen
+	 * dargestellt. Die Elemente index, Frage und Antwort werden durch Semikolons
+	 * getrennt.
+	 */
 	void saveDeckCSV() {
 
 		Path pathCSV = Paths.get(DeckOrganizer.getPathtoString(), deckname + ".csv");
@@ -67,7 +102,6 @@ public class Deck {
 			if (getAmountOfFlashcards() > 0) {
 				for (Flashcard f : flashcardList) {
 					String questionString = f.getQuestion().replace('\n', '\\');
-					System.out.println("HIER:" + questionString);
 					String answerString = f.getAnswer().replace('\n', '\\');
 					String row = String.format("%d;%s;%s%n", flashcardList.indexOf(f), questionString, answerString);
 
@@ -77,10 +111,13 @@ public class Deck {
 			writeBuffer.close();
 
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println(e);
 		}
 	}
 
+	/**
+	 * Löscht die CSV Datei aus dem Ordner.
+	 */
 	void deleteDeckCSV() {
 		Path pathCSV = Paths.get(DeckOrganizer.getPathtoString(), deckname + ".csv");
 		try {
@@ -90,20 +127,50 @@ public class Deck {
 		}
 	}
 
+	/**
+	 * Gibt die Anzahl der Karteikarten des Decks wieder.
+	 * 
+	 * @return Karteikartenanzahl
+	 */
 	int getAmountOfFlashcards() {
 		return flashcardList.size();
 	}
 
+	/**
+	 * Gibt die Liste der Karteikarten wieder.
+	 * 
+	 * @return Liste Karteikarten
+	 */
 	List<Flashcard> getDeckFlashcardlist() {
 		return flashcardList;
 	}
 
+	/**
+	 * Gibt den Deckname wieder.
+	 * 
+	 * @return Deckname
+	 */
 	String getDeckname() {
 		return deckname;
 	}
 
+	/**
+	 * Setzt den Deckname neu
+	 * 
+	 * @param deckname neuer Deckname
+	 */
 	void setDeckname(String deckname) {
 		this.deckname = deckname;
+	}
+
+	// checkt ob der Text null oder leer ist. Sonst wirft er eine Exeption
+	private boolean checkQAText(String text) throws UnvalidQAException {
+
+		if (!text.isBlank() && text != null) {
+			return true;
+		} else {
+			throw new UnvalidQAException();
+		}
 	}
 
 	@Override
--- ptp-lernkarten/src/model/Flashcard.java
+++ ptp-lernkarten/src/model/Flashcard.java
@@ -1,29 +1,59 @@
 package model;
 
+/**
+ * * PTP 22- Karteikartenlernprogramm
+ * 
+ * Die Klasse Flashcard ist für die Speicherung der Werte Frage und Antwort
+ * einer Karteikarte zuständig.
+ * 
+ * @author J.Dillmann, M. Sterkel
+ */
 public class Flashcard {
 
-	private String question;
-	private String answer;
+	private String question; // Frage
+	private String answer; // Antwort
 
+	/**
+	 * Im Konstruktor werden Frage und Antwort initialisiert
+	 */
 	public Flashcard(String q, String a) {
-//		this.index = index;
 		this.question = q;
 		this.answer = a;
 
 	}
 
+	/**
+	 * Gibt die Frage wieder.
+	 * 
+	 * @return Frage
+	 */
 	String getQuestion() {
 		return question;
 	}
 
+	/**
+	 * Setzt die Frage neu.
+	 * 
+	 * @param question Frage
+	 */
 	void setQuestion(String question) {
 		this.question = question;
 	}
 
+	/**
+	 * Gibt die Antwort wieder.
+	 * 
+	 * @return Antwort
+	 */
 	String getAnswer() {
 		return answer;
 	}
 
+	/**
+	 * Setzt die Antwort neu.
+	 * 
+	 * @param answer Antwort
+	 */
 	void setAnswer(String answer) {
 		this.answer = answer;
 	}
